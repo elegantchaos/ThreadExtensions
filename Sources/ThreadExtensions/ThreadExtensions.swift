53e17a23bae8af73bd03be966e39b3b3ffa78421
Sam Deane <sam@elegantchaos.com>
2021-01-12 17:28:10 +0000

    added onMainQueue

--- Sources/ThreadExtensions/ThreadExtensions.swift
+++ Sources/ThreadExtensions/ThreadExtensions.swift
@@ -1,8 +1,15 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-//  Created by Sam Developer on 12/01/2021.
-//  All code (c) 2021 - present day, Sam Developer.
+//  Created by Sam Deane on 12/01/2021.
+//  All code (c) 2021 - present day, Elegant Chaos Limited.
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-struct ThreadExtensions {
-    var text = "Hello, World!"
+import Foundation
+
+public func onMainQueue(_ work: @escaping () -> ()) {
+    DispatchQueue.main.async(execute: work)
 }
+
+public func onMainQueue(after seconds: TimeInterval, _ work: @escaping () -> ()) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: work)
+}
+
